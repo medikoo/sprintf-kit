4294359ced874fe9ca30b515bfb28a2876ce4fdf
Mariusz Nowak <medyk@medikoo.com>
2018-05-24 17:06:52 +0200

    feat: improve patch message

--- modifiers/j.js
+++ modifiers/j.js
@@ -15,7 +15,9 @@ module.exports = function (value/*, placeholder, argIndex, args*/) {
 	try {
 		return JSON.stringify(value, null, 2);
 	} catch (e) {
-		if (e.message === CIRCULAR_JSON_ERROR_MESSAGE) return "[Circular JSON]";
+		if (e.message === CIRCULAR_JSON_ERROR_MESSAGE) {
+			return "<Circular (non-JSON serializable) value>";
+		}
 		return "<Non-serializable (to JSON) value>";
 	}
 };

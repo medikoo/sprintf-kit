b51f5d86c75faba5c61221ca4d4dc8aece157d43
Mariusz Nowak <medyk@medikoo.com>
2018-08-03 21:32:28 +0200

    refactor: split into functions

--- index.js
+++ index.js
@@ -13,61 +13,70 @@ var slice = Array.prototype.slice;
 
 var customTypes = primitiveSet("literal", "rest");
 
-module.exports = function (modifiers) {
+var validateModifiers = function (modifiers) {
 	objForEach(ensureObject(modifiers), function (value, type) {
 		if (!hasOwnProperty.call(typeChars, type) && !customTypes[type]) {
 			throw new TypeError("Invalid modifier type: " + type);
 		}
 		ensurePlainFunction(value);
 	});
+};
+
+var resolveRest = function (modifier, data, args, placeholderArgsLength) {
+	if (!modifier || args.length <= placeholderArgsLength) return "";
+	return modifier(args.slice(placeholderArgsLength), data);
+};
+
+var resolveStringWithPlaceholders = function (modifiers, data, args) {
+	var extraPlaceholderArgsLength = 0;
+	var isParameterIndexingValid = data.isParameterIndexingValid;
+	var resolvedString = data.literals.reduce(function (result, literal, index) {
+		if (index === 1 && modifiers.literal) result = modifiers.literal(result, args);
+		var placeholder = data.placeholders[index - 1];
+		var resolvedPlaceholder;
+		if (placeholder.width === "*") ++extraPlaceholderArgsLength;
+		if (placeholder.precision === "*") ++extraPlaceholderArgsLength;
+		if (!isParameterIndexingValid) {
+			resolvedPlaceholder = "[invalid placeholder parameters]";
+		} else if (modifiers[placeholder.type]) {
+			if (placeholder.parameter) index = placeholder.parameter;
+			if (index > args.length) {
+				resolvedPlaceholder = placeholder.content;
+			} else {
+				index += extraPlaceholderArgsLength - 1;
+				var arg = args[index];
+				resolvedPlaceholder = modifiers[placeholder.type](arg, placeholder, index, args);
+			}
+		} else {
+			resolvedPlaceholder = placeholder.content;
+		}
+		return result + resolvedPlaceholder + (modifiers.literal || identity)(literal, args);
+	});
+	return (
+		resolvedString +
+		resolveRest(
+			modifiers.rest, data, args, data.placeholders.length + extraPlaceholderArgsLength
+		)
+	);
+};
+
+var resolveStringWithoutPlaceholders = function (modifiers, data, args) {
+	var resolvedString = data.literals[0];
+	if (modifiers.literal) resolvedString = modifiers.literal(resolvedString);
+	return resolvedString + resolveRest(modifiers.rest, data, args, data.placeholders.length);
+};
+
+module.exports = function (modifiers) {
+	validateModifiers(modifiers);
 
 	return function (format/*, ...params*/) {
 		if (typeof format !== "string") {
 			return modifiers.rest ? modifiers.rest(aFrom(arguments)) : "";
 		}
 		var data = parse(format);
-		var placeholders = data.placeholders;
-		var isParameterIndexingValid = data.isParameterIndexingValid;
-
 		var args = slice.call(arguments, 1);
-		var skippedArgsLength = 0;
-		var resolvedString;
-		if (data.literals.length > 1) {
-			resolvedString = data.literals.reduce(function (result, literal, index) {
-				if (index === 1 && modifiers.literal) result = modifiers.literal(result, args);
-				var placeholder = placeholders[index - 1];
-				var resolvedPlaceholder;
-				if (placeholder.width === "*") ++skippedArgsLength;
-				if (placeholder.precision === "*") ++skippedArgsLength;
-				if (!isParameterIndexingValid) {
-					resolvedPlaceholder = "[invalid placeholder parameters]";
-				} else if (modifiers[placeholder.type]) {
-					if (placeholder.parameter) index = placeholder.parameter;
-					if (index > args.length) {
-						resolvedPlaceholder = placeholder.content;
-					} else {
-						index += skippedArgsLength - 1;
-						var arg = args[index];
-						resolvedPlaceholder = modifiers[placeholder.type](
-							arg, placeholder, index, args
-						);
-					}
-				} else {
-					resolvedPlaceholder = placeholder.content;
-				}
-				return (
-					result + resolvedPlaceholder + (modifiers.literal || identity)(literal, args)
-				);
-			});
-		} else {
-			resolvedString = (modifiers.literal || identity)(data.literals[0]);
-		}
-		if (!modifiers.rest || args.length <= placeholders.length + skippedArgsLength) {
-			return resolvedString;
-		}
-		return (
-			resolvedString +
-			modifiers.rest(args.slice(placeholders.length + skippedArgsLength), data)
-		);
+
+		if (data.literals.length > 1) return resolveStringWithPlaceholders(modifiers, data, args);
+		return resolveStringWithoutPlaceholders(modifiers, data, args);
 	};
 };
